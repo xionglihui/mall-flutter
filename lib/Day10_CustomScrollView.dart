a44f4755b478f97f2770c1ed7f1b3c15d2f1ec20
xionglihui <xionglihui@vip.qq.com>
2020-10-29 13:43:26 +0800

    CustomScrollView

--- lib/Day10_CustomScrollView.dart
+++ lib/Day10_CustomScrollView.dart
@@ -10,6 +10,44 @@ class CustomScrollViewTest extends StatelessWidget {
           SliverAppBar(
             pinned: true,
             expandedHeight: 250,
+            flexibleSpace: FlexibleSpaceBar(
+              title: const Text("Demo"),
+              background: Image.asset("assets/images/ic_launcher.png",fit: BoxFit.cover,),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(8),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing:10,
+                childAspectRatio: 4.0,
+                crossAxisSpacing: 10,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                  (BuildContext buidContext,int index){
+                    return Container(
+                      alignment: Alignment.center,
+                      color: Colors.cyanAccent[100*(index%9)],
+                      child: Text("grid item $index"),
+                    );
+                  },
+                childCount: 20
+              ),
+            )
+          ),
+          SliverFixedExtentList(
+            itemExtent: 50.0,
+            delegate: SliverChildBuilderDelegate(
+                (BuildContext buidContext,int index){
+                  return  Container(
+                    alignment: Alignment.center,
+                    color: Colors.lightBlue[100*(index%9)],
+                    child: Text("list item $index"),
+                  );
+                },
+              childCount: 50
+            ),
           )
         ],
       ),
