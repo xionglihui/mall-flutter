fff5c87f60a9e647a441d75abfc2bad83f23aaf2
xionglihui <xionglihui@vip.qq.com>
2020-11-02 11:35:27 +0800

    InheritedWidget

--- lib/view/test/Day11_InheritedWidget.dart
+++ lib/view/test/Day11_InheritedWidget.dart
@@ -1,13 +1,81 @@
+import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class InheritedWidgetTest extends InheritedWidget{
+class ShareDataWight extends InheritedWidget {
+  static ShareDataWight of(BuildContext buildContext) {
+    //定义一个便捷的方法，方便子树中的wight 获取共享数据
+    return buildContext.dependOnInheritedWidgetOfExactType<ShareDataWight>();
+  }
 
+  final int data; //需要在再子树中共享的数据 ，保存点击次数
+
+  ShareDataWight({@required this.data, Widget child}) : super(child: child);
 
   @override
-  bool updateShouldNotify(InheritedWidget oldWidget) {
-    // TODO: implement updateShouldNotify
-    throw UnimplementedError();
+  bool updateShouldNotify(ShareDataWight oldWidget) {
+    return oldWidget.data != data;
   }
-  
+}
+
+class InheritedWidgetTest1 extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => InheritedWidgetTestStatus();
+}
+
+class InheritedWidgetTestStatus extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Text(ShareDataWight.of(context).data.toString());
+  }
+}
+
+class InheritedWidgetTest extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => InheritedWidgetTestStatue();
+}
+
+class InheritedWidgetTestStatue extends State {
+  int data = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("InheritedWidgetTest"),
+        ),
+        body: Center(
+          child: ShareDataWight(
+            data: data,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: InheritedWidgetTest1(),
+                ),
+                RaisedButton(
+                  child: Text(
+                    "onclick",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  color: Colors.blue,
+                  onPressed: () => {
+                    setState(() {
+                      ++data;
+                    })
+                  },
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("didChangeDependencies");
+  }
 }
